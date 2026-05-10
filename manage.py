dca239bbf02d5c74bd67c00c232d958f35979402
Harsh Vishwakarma <your_email@gmail.com>
2026-05-10 16:59:34 +0530

    Fix merge conflict in manage file

--- manage.py
+++ manage.py
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 #!/usr/bin/env python
 import os
 import sys
@@ -12,19 +11,4 @@ def main():
 
 
 if __name__ == "__main__":
-=======
-#!/usr/bin/env python
-import os
-import sys
-
-
-def main():
-    os.environ.setdefault("DJANGO_SETTINGS_MODULE", "documentauth.settings")
-    from django.core.management import execute_from_command_line
-
-    execute_from_command_line(sys.argv)
-
-
-if __name__ == "__main__":
->>>>>>> dc6517ec272cf5ca0aee63c8db5b7412ca1fa5a0
     main()
